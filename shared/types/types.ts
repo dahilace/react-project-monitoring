b8940b84e6aec39c28f80d3ad709dd98f54320ec
Данил Семухин <b1gbywa@gmail.com>
2026-03-18 04:45:19 +0500

    feat(shared): add core types for app (IUser, ITask)

---
+++ shared/types/types.ts
@@ -0,0 +1,26 @@
+type UserRole = 'worker' | 'manager'
+type TaskPriority = 'standart' | 'high' | 'low'
+type TaskStatus = 'appointed' | 'started' | 'ended' | 'declined'
+
+
+export interface IUser {
+  name: string
+  surname: string | null
+  fatherName: string | null
+  login: string
+  password: string
+  manager: string | null
+  role: UserRole
+}
+
+export interface ITask {
+  title: string
+  description: string
+  dateOfEnd: Date | null
+  dateOfCreation: Date
+  dateOfUpdate: Date | null
+  priority: TaskPriority
+  status: TaskStatus
+  creator: string
+  responsible: string
+}
