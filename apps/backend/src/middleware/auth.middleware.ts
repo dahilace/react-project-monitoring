c6c42102f80ad50f287e15572cd8e0d809752053
Данил Семухин <b1gbywa@gmail.com>
2026-03-22 06:27:34 +0500

    fix(auth): replace JWT_SECRET to .env

--- apps/backend/src/middleware/auth.middleware.ts
+++ apps/backend/src/middleware/auth.middleware.ts
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
-const JWT_SECRET = 'supersecret'
+const JWT_SECRET = process.env.JWT_SECRET
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization
