4f4eab477358c150c286ba19d02b7f0ce5313c17
Данил Семухин <b1gbywa@gmail.com>
2026-03-23 08:56:41 +0500

    feat(route) add /me route to identify user data

--- apps/backend/src/routes/auth.route.ts
+++ apps/backend/src/routes/auth.route.ts
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
-
+const authMiddleware = require('../middleware/auth.middleware')
 const authController = require('../controllers/auth.controller')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 router.post('/login', authController.login)
+
+router.get('/me', authMiddleware, async (req, res) => {
+  const userId = req.user.userId
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      name: true,
+      login: true,
+      role: true
+    }
+  })
+
+  res.json(user)
+})
+
 
 module.exports = router
